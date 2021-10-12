b0a0e82ea7e84620d3c004123f5ca94e52288f5b
wilberchair <wilberchair@gmail.com>
2021-10-12 17:45:59 -0300

    sintax sugar

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,17 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  // private negociacoes: Array<Negociacao> = [];
+  //Posso fazer da forma abaixo como abreviação
+  private negociacoes: Negociacao[] = [];
 
   adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
-  lista(): ReadonlyArray<Negociacao> {
+  // lista(): ReadonlyArray<Negociacao> {
+  //sintax sugar forma abreviada
+  lista(): readonly Negociacao[] {
     return this.negociacoes;
   }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,10 +1,14 @@
 export class Negociacoes {
     constructor() {
+        // private negociacoes: Array<Negociacao> = [];
+        //Posso fazer da forma abaixo como abreviação
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // lista(): ReadonlyArray<Negociacao> {
+    //sintax sugar forma abreviada
     lista() {
         return this.negociacoes;
     }
