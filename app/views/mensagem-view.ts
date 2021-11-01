f522a06e4a67ab67940d19593ebdaf93a5b8aa1f
wilberchair <wilberchair@gmail.com>
2021-11-01 13:24:47 -0300

    ajustes na view de mensagem

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,18 +1,10 @@
-export class MensagemView {
+import { View } from "./view.js";
 
-  private elemento: HTMLElement;
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor)
-  }
+export class MensagemView extends View<string>{
 
   template(model: string): string {
     return `
       <p class="alert alert-info">${model}</p>
     `
-  }
-
-  update(model: string): void {
-    const template = this.template(model);
-    this.elemento.innerHTML = template;
   }
 }
